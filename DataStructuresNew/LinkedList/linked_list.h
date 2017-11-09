01c766adda516aa5c0d360b3f7a91434dcf86a90
rohithrajreganti <rohithrajreganti@gmail.com>
2017-11-09 21:45:42 +0530

    modified

--- DataStructuresNew/LinkedList/linked_list.h
+++ DataStructuresNew/LinkedList/linked_list.h
@@ -5,6 +5,7 @@ typedef struct node{
 
     int data;
     struct node* next;
+    int number;
 
 
 }Node;
@@ -18,8 +19,14 @@ Node* create_list(int val){
 
 }
 
-void insert_back(Node* header,int val){
-    
+Node* insert_back(Node* header,int val){
+     
+    if(header==NULL){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data=val;
+    new_node->number=1;
+    return new_node;
+    }
     Node* temp = header;
     while(temp->next != NULL){
 
@@ -32,6 +39,8 @@ void insert_back(Node* header,int val){
     new_node->next=NULL;
     new_node->data=val;
     temp->next = new_node;
+    return header;
+
 
 }
 
@@ -47,12 +56,19 @@ void display(Node* header){
 }
 
 Node* insert_front(Node* header,int val){
-
+    
+    if(header==NULL){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data=val;
+    new_node->number=1;
+    return new_node;
+    }
     
     Node* new_node = (Node*)malloc(sizeof(Node));
     new_node->data=val;
 
     new_node->next= header;
+    new_node->number= header->number+1;
     return new_node;
 
 }
@@ -66,7 +82,7 @@ int delete_front(Node** header){
 
 }
 
-int delete_rear(Node* header){
+int delete_back(Node* header){
     
     
     Node* temp = header;
