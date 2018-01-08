75154e4f344cfef09c2aa5818ded0c65f32650b9
rohithrajreganti <rohithrajreganti@gmail.com>
2018-01-08 23:58:28 +0530

    added sorting with binary_predicate

---
+++ holiday_practice/vector_demo/own_sort_pairs.cpp
@@ -0,0 +1,76 @@
+#pragma warning(disable:4786)
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<functional>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<utility>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<stack>
+#include<assert.h>
+using namespace std;
+
+
+
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+#define IN(A, B, C)  (B) <= (A) && (A) <= (C)
+
+
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+
+#define AIN(A, B, C) assert(IN(A, B, C))
+
+//typedef int LL;
+//typedef long long int LL;
+//typedef __int64 LL;
+
+bool predic(const pair<int,string>& p1, const pair<int,string>& p2){
+
+   if(p1.second <p2.second) return false ; 
+   else return true;
+
+}
+int main()
+{
+
+ios_base::sync_with_stdio(0);
+
+vector< pair<int,string> > v;
+
+v.push_back(make_pair(1,"z"));
+v.push_back(make_pair(2,"y"));
+v.push_back(make_pair(3,"x"));
+
+sort(v.begin(),v.end(),predic);
+
+for(auto& x:v){
+    
+    cout<<x.first<< " "<<x.second<<endl;
+
+}
+
+
+
+
+return 0;
+}
+
