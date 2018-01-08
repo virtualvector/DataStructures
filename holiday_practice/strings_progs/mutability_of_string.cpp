1ca2a2ff5d407cd5070c24c5e5ba8e1f35ebaecc
rohithrajreganti <rohithrajreganti@gmail.com>
2018-01-08 23:48:51 +0530

    comm

---
+++ holiday_practice/strings_progs/mutability_of_string.cpp
@@ -0,0 +1,76 @@
+#pragma warning(disable:4786)
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<functional>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<utility>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<stack>
+#include<assert.h>
+using namespace std;
+
+
+
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+#define IN(A, B, C)  (B) <= (A) && (A) <= (C)
+
+
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+
+#define AIN(A, B, C) assert(IN(A, B, C))
+
+//typedef int LL;
+//typedef long long int LL;
+//typedef __int64 LL;
+
+int main()
+{
+
+ios_base::sync_with_stdio(0);
+
+string s = "hello";
+
+char s1[] = "char string you know";
+
+
+string s2(s1);
+string s3(&s1[0]);
+
+cout<<s1<<endl;
+s2[1]='z';
+cout<<s2<<" "<< s1<<endl;
+
+s3[1]='y';
+cout<<s3<<" "<<s1<<endl;
+
+
+string s4=s1;
+s4[1]='p';
+cout<<s1<<endl;
+
+
+
+
+return 0;
+}
+
