47a7d4af3a03e610f08689223eb5272100c3b145
rohithrajreganti <rohithrajreganti@gmail.com>
2018-01-08 21:54:31 +0530

    auto_ptr demo

---
+++ holiday_practice/auto_ptrdemo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+void call_func(){
+
+    auto_ptr<double> ptr(new double);
+
+
+}
+
+
+int main(){
+
+    call_func();
+
+return 0;
+}
