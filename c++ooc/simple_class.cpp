35c9a76e368b90746891864f2d31aa08b9b00a18
rohithrajreganti <rohithrajreganti@gmail.com>
2018-02-09 17:45:54 +0530

    simple class

---
+++ c++ooc/simple_class.cpp
@@ -0,0 +1,51 @@
+#pragma warning(disable:4786)
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<functional>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<utility>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<stack>
+#include<assert.h>
+using namespace std;
+
+class SimpleClass
+{
+    public:
+        int a; string s;
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+ios_base::sync_with_stdio(0);
+SimpleClass sc;
+sc.s = "hello";
+cout<<sc.s<<endl;
+
+
+
+return 0;
+}
+
