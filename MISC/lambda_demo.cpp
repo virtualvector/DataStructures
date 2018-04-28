6cdcc6d7ad6494c314e7a0d64449f676157bb18f
rohithrajreganti <rohithrajreganti@gmail.com>
2018-04-28 15:09:57 +0530

    g++14 addition

--- MISC/lambda_demo.cpp
+++ MISC/lambda_demo.cpp
@@ -9,7 +9,7 @@ int main()
     vector<int> v(100);
     for(int i=0;i<v.size();i++) v[i]=i;
 
-    sort(v.begin(),v.end(), [](int a,int b)->bool { return a>b; });
+    sort(v.begin(),v.end(), [](auto a,auto b)->bool { return a>b; });
     cout<<endl;
     for_each(v.begin(),v.end(), [](int a)->float{ cout<<a<<" ";} );
 
