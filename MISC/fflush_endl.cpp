f5acf211b8f79e7ea2709b852189f3300fd5c522
rohithrajreganti <rohithrajreganti@gmail.com>
2018-05-14 21:32:57 +0530

    flflush

---
+++ MISC/fflush_endl.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+
+    while(true)
+    {
+    cout<<"helloin\n";
+    printf("hello2");
+//    fflush(stdout);
+
+    int* a;
+    *a = 2;
+
+
+    }
+
+
+
+
+}
